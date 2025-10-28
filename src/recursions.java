a8ab971a507f0268cd0a587e96a76aff3708ca18
vOlive2.0 <OLiveA148@lpsk12.org>
2025-10-28 08:32:49 -0600

    Update recursions.java

--- src/recursions.java
+++ src/recursions.java
@@ -8,6 +8,7 @@ public class recursions {
 		System.out.println(sumDigits(123)); // 6
 		System.out.println(sumDigits(12345)); // 15
 		System.out.println(sumDigits(954736163)); // 44
+		System.out.println(binaryRep()); // 1
 	}
 	public static int gcd(int a, int b) {		
 		if(a < b) {int c=a; a=b; b=c;}
@@ -23,10 +24,10 @@ public class recursions {
 		return b + sumDigits(a);
 	}
 	public static int binarySearch(int[] arr, int target) {	
-		return b;
+		return 0;
 	}
-	public static int binaryRep(int a) {	
-		int b = a;
-		return b;
+	public static String binaryRep(int a) {	
+		if(a == 0 || a == 1) return ""+a;
+		return binaryRep(a/2)+binaryRep(a%2);
 	}
 }
